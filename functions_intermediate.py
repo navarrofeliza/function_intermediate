521e37e32eb5c2e2a36853ce8ba8b0704a4e3747
navarrofeliza <navarrofeliza@yahoo.com>
2022-01-10 22:05:25 -0800

    innitial commit

---
+++ functions_intermediate.py
@@ -0,0 +1,46 @@
+# Update Values in Dictionaries and Lists
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+students[0]['last_name'] = "Bryant"
+sports_directory['soccer'][0] = "Andres"
+z[0]['y'] = 30
+
+# Iterate Through a List of Dictionaries
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+for i in range (len(students)):
+    print(students[i])
+    
+# Get Values From a List of Dictionaries
+def iterateDictionary(name,list):
+    for i in list:
+        print(i[name])
+iterateDictionary('first_name', students)
+iterateDictionary('last_name', students)
+
+# Iterate Through a Dictionary with List Values
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(dict):
+    for i in dojo:
+        print(len(dojo[i]))
+        print(i)
+        for name in dojo[i]:
+            print (name)
+printInfo(dojo)
